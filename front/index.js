d726df3096a9be34f2c0e4f7f1a7d0beae324257
Alberto Garcia <albertoinformatica97@gmail.com>
2019-08-20 19:11:57 +0200

    Ya se devuelve array con las respuestas y se avisa cuando se han recibido todas

--- front/index.js
+++ front/index.js
@@ -1,12 +1,39 @@
 console.log('hello')
+let arrayResponses = []
 
 function getImages() {
 
-    fetch('http://localhost:3000/images/los%20simpsons')
-    .then(res => res.json())
-    .then(resJSON => console.log(resJSON.response))
-    .catch(err => {
-        console.log(err)
-    })
+    
+    document.getElementById('msgCargando').textContent = 'Cargando...'
+    arrayResponses = []
+
+    let arrayImages = [
+        'los%20simpsons',
+        'padre%20de%20familia',
+        'american%20dad'
+    ]
+
+    for (let i = 0; i < arrayImages.length; i++) {
+
+        fetch(`http://localhost:3000/images/${arrayImages[i]}`)
+        .then(res => res.json())
+        .then(resJSON => {
+            console.log(resJSON.response)
+            arrayResponses.push(resJSON.response)
+            if (i === arrayImages.length - 1) {
+                document.getElementById('msgCargando').textContent = 'Imagenes listas!'
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+    }
+
+}
+
+function displayImages() {
+
+    console.log(arrayResponses)
 
 }
