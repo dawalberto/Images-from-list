9e74b406e6dc5eacd6e315ee0eecc88c1486dc4c
Alberto Garcia <albertoinformatica97@gmail.com>
2019-08-21 13:45:10 +0200

    La idea principal ya funciona, ahora hay que ponerlo bonito y agregar un par de funcionalidades

--- front/index.js
+++ front/index.js
@@ -8,6 +8,7 @@ function getArrayOfTextarea() {
 
 function getImages() {
 
+    document.getElementById('container-res').textContent = ''
     document.getElementById('msgCargando').textContent = 'Cargando...'
     document.getElementById('btnDisplayImages').disabled = true
     arrayResponses = []
@@ -18,18 +19,17 @@ function getImages() {
     //     'american%20dad'
     // ]
 
-    let arrayImages = getArrayOfTextarea()
+    let arraySearchImages = getArrayOfTextarea()
 
-    for (let i = 0; i < arrayImages.length; i++) {
+    for (let i = 0; i < arraySearchImages.length; i++) {
 
-        fetch(`http://localhost:3000/images/${arrayImages[i]}`)
+        fetch(`http://localhost:3000/images/${arraySearchImages[i]}`)
         .then(res => res.json())
         .then(resJSON => {
-            // resJSON.response.searchImage = arrayImages[i]
-            resJSON.response.push(arrayImages[i])
+            resJSON.response.push(arraySearchImages[i])
             console.log(resJSON.response)
             arrayResponses.push(resJSON.response)
-            if (i === arrayImages.length - 1) {
+            if (i === arraySearchImages.length - 1) {
                 document.getElementById('msgCargando').textContent = 'Imagenes listas!'
                 document.getElementById('btnDisplayImages').disabled = false
             }
@@ -44,15 +44,40 @@ function getImages() {
 
 function displayImages() {
 
+    let searchImage;
+    let image;
+    let div;
+    let col;
+    let arrayDivs;
+    let containerRes = document.getElementById('container-res')
     console.log(arrayResponses)
 
     for (let i = 0; i < arrayResponses.length; i++) {
+        arrayDivs = []
         for(let x = 0; x < arrayResponses[i].length; x++) {
             console.log('arrayResponses[i][x]', arrayResponses[i][x])
             if (typeof arrayResponses[i][x] === 'string') {
                 console.log('palabra busqueda imagen', arrayResponses[i][x])
+                searchImage = document.createElement('h1')
+                searchImage.classList.add('col-1-3')
+                searchImage.classList.add('justify-self-center')
+                searchImage.textContent = arrayResponses[i][x]
+                containerRes.appendChild(searchImage)
+            } else {
+                image = document.createElement('img')
+                image.src = arrayResponses[i][x].url
+                image.classList.add('w-100')
+                // image.width = 200
+                div = document.createElement('div')
+                col = `col-${x+1}`
+                div.classList.add(col)
+                div.classList.add('justify-self-center')
+                div.appendChild(image)
+                arrayDivs.push(div)
             }
+            arrayDivs.map(div => containerRes.appendChild(div))
         }
+
     }
 
 }
